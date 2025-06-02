c248a46c206480b28c7617e92661dc0da553097f
USER <rhs9827@naver.com>
2025-06-02 10:31:14 +0900

    operator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+
 public class Main {
     public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        int sum = a + b;
+        System.out.println(sum);
+
+        int defference = a - b;
+        System.out.println(defference);
+
+        int product = a * b;
+        System.out.println(product);
+
+        int quotient = a / b;
+        System.out.println(quotient);
+
+        int remainder = a % b;
+        System.out.println(remainder);
 
 
         }
