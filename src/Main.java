fe311c15fa3fe68475b0926c745490cc321bd580
USER <rhs9827@naver.com>
2025-06-02 11:24:13 +0900

    Increment/decrement operator

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,9 @@ public class Main {
         int a = 10;
         int b = 3;
 
-        a += 5;
-        System.out.println(a);
+        System.out.println(a++);
 
-//        b -= 1;
-//        System.out.println(b);
-//        b *= 2;
-//        System.out.println(b);
-//        b /= 3;
-//        System.out.println(b);
-//        b %= 4;
-//        System.out.println(b);
+        System.out.println(b--);
+
         }
     }
