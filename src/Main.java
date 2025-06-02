368781938c83567973d33036a07c2e0fcfd6cd27
USER <rhs9827@naver.com>
2025-06-02 14:45:07 +0900

    switch

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        int number = 0;
+//        "happy", :angry", "sad", etc.
+        String mood = "";
 
-        if(number > 0){
-            System.out.println("number은 양수입니다.");
-        } else if (number < 0){
-            System.out.println("number은 음수입니다.");
-        } else{
-            System.out.println("number은 0입니다.");
+        switch (mood) {
+            case "happy":
+                System.out.println("나는 행복하다. 왜냐하면 행복하기 때문이다.");
+                break;
+            case "tired":
+                System.out.println("피곤하다 너무 자고싶다.");
+                break;
+            case "sad":
+                System.out.println("수업중이라 잘 수가 없어 슬프다.");
+                break;
+            case "angry":
+                System.out.println("지금 잘 수가 없어 화가난다.");
+                break;
+
+            default:
+                System.out.println("지금 내 기분은 이 단어들로는 표현할 수 없다.");
         }
     }
 }
