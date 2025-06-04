415925c13ddafb1e63883f4a87407abd08910844
USER <rhs9827@naver.com>
2025-06-04 16:22:13 +0900

    6-1(5)

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int input =1;
-
-        do {
-            System.out.println("안녕하세요 자판기입니다");
-            switch (input) {
-                case 1:
-                    System.out.println("콜라");
-                    break;
-                case 2:
-                    System.out.println("사이드");
-                    break;
-                case 3:
-                    System.out.println("물");
-                    break;
-                default:
-                    System.out.println("잘못고름");
-            }
-
-        } while (input == 0);
+        int number = 1111;
+        String binaryString = Integer.toBinaryString(number);
+        String hexString = Integer.toHexString(number);
+        System.out.println("number = " + number);
+        System.out.println("binary = " + binaryString);
+        System.out.println("hex = " + hexString);
     }
 }
